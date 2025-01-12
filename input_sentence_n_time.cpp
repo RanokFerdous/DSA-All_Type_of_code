90b31a43545619b2bf910031910e671005d1083c
RanokFerdous <164327286+RanokFerdous@users.noreply.github.com>
2025-01-12 13:12:40 +0600

    Create input_sentence_n_time.cpp

---
+++ input_sentence_n_time.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for( int i=0;i<5;i++)
+    {
+    string s;
+    cin.ignore();
+    getline(cin,s);
+    cout<<s;
+    }
+
+    return 0;
+}
